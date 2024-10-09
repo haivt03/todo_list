81674973866586dc0f0df9c25845efd6bb5d3c6f
haivt03 <haivt0306@gmail.com>
2024-10-09 13:41:23 +0700

    add hook

---
+++ todo-list/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 text-white">
+      <div className="container mx-auto text-center">
+        <p> 2024 Todo App. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- todo-list/src/types/auth/auth.type.ts
+++ todo-list/src/types/auth/auth.type.ts
@@ -1,10 +1,16 @@
-export interface AuthPayload {
-    username: string;
-    password: string;
-  }
-  
-  export interface LoginResponse {
-    id: number;
-    username: string;
-  }
-  
+export interface AuthLogin {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  id: number;
+  username: string;
+}
+export interface AuthRegister {
+  firstNames: string;
+  lastNames: string;
+  ages: number;
+  usernames: string;
+  passwords: string;
+}
