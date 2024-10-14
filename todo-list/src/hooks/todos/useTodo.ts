493a9e02861db1a4112a07c799544134197563e0
haivt03 <haivt0306@gmail.com>
2024-10-14 14:13:58 +0700

    fix: change to mutation

--- todo-list/src/hooks/todos/useTodo.ts
+++ todo-list/src/hooks/todos/useTodo.ts
@@ -1,5 +1,4 @@
-import { useQuery } from "react-query";
-import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { addTodos, fetchTodosByUserId } from "../../api/todo";
 import { Todo } from "../../types/todos/todos.type";
 
@@ -29,25 +28,27 @@ export function useTodos() {
 }
 
 export function useAddTodo() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | string>(null);
-
-  const handleAddTodo = async (title: string, completed: boolean) => {
-    setIsLoading(true);
-    try {
+  const mutation = useMutation<Todo, Error, { title: string; completed: boolean }>({
+    mutationFn: async ({ title, completed }) => {
       const userId = localStorage.getItem("userId");
       if (!userId) {
         throw new Error("No userId found in localStorage");
       }
+      return await addTodos(title, completed, Number(userId));
+    },
+    onError: (error) => {
+      console.error("Failed to add todo", error.message);
+    },
+  });
 
-      const response = await addTodos(title, completed, Number(userId));
-      setIsLoading(false);
-      return response;
-    } catch (err) {
-      setIsLoading(false);
-      setError("Failed to add todo");
-    }
+  const handleAddTodo = (title: string, completed: boolean) => {
+    mutation.mutate({ title, completed });
   };
 
-  return { handleAddTodo, isLoading, error };
+  return {
+    handleAddTodo,
+    isLoading: mutation.isLoading,
+    isError: mutation.isError,
+    error: mutation.error,
+  };
 }
